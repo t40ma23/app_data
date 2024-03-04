8c37ac5624a328e2900a6ea0d7fbe24712f77f77
MUHAMMAD ARSHAD <m.arshad.23@abdn.ac.uk>
2024-03-04 06:49:18 +0000

    Updated code in the app_data directory

--- population.py
+++ population.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def get_country_data(country):
     conn = sqlite3.connect('population_data.db')
     c = conn.cursor()
-    c.execute("SELECT * FROM your_table WHERE country_name=?", (country,))
+    c.execute("SELECT * FROM population_data WHERE country_name=?", (country,))
     data = c.fetchall()
     conn.close()
     return data
@@ -15,23 +15,27 @@ def get_country_data(country):
 # Define route for the homepage displaying Italy data
 @app.route('/')
 def index():
-    # Get data for Italy
+    return render_template ('index.html')
+
+# Get data for Italy
+@app.route('/italy')
+def italy():
     italy_data = get_country_data('Italy')
-    return render_template('index.html', country='Italy', data=italy_data)
+    return render_template('country.html', country='Italy', data=italy_data)
 
 # Define route for Korea data
 @app.route('/korea')
 def korea():
     # Get data for Korea
-    korea_data = get_country_data('Korea')
-    return render_template('index.html', country='Korea', data=korea_data)
+    korea_data = get_country_data('Korea, Rep.')
+    return render_template('country.html', country='Korea, Rep.', data=korea_data)
 
 # Define route for Japan data
 @app.route('/japan')
 def japan():
     # Get data for Japan
     japan_data = get_country_data('Japan')
-    return render_template('index.html', country='Japan', data=japan_data)
+    return render_template('country.html', country='Japan', data=japan_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
